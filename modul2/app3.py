29133292f7f7c9bebca9e4a62fecffbbf622ad3e
Iulian Dabuleanu <iulian.dabuleanu@@teraseya.com>
2024-01-29 21:04:22 +0200

    MODUL 2

---
+++ modul2/app3.py
@@ -0,0 +1,9 @@
+
+
+password = 7710
+user_pass = input('Enter password: ')
+print(user_pass)
+
+print(str(password) == user_pass)
+print(str(password) == int(user_pass))
+
---
+++ modul2/part1_3.py
@@ -0,0 +1,15 @@
+# COMPARE
+
+print(10 <= 11)
+print(type(10 > 11))
+
+print(10 == 11)
+print(type(10 == 11))
+
+
+
+
+# bool
+print(False)
+print(True)
+
